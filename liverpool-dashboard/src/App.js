8e1a17ff7a36c6fded04ad34374b67d689bf039a
whambamleo <89803624+whambamleo@users.noreply.github.com>
2022-08-16 21:43:00 -0500

    trying first deployment

--- liverpool-dashboard/src/App.js
+++ liverpool-dashboard/src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
+import "./App.css";
+
+// Get the current first team
+function FirstTeam(props) {
+      var myHeaders = new Headers();
+
+      myHeaders.append("x-rapidapi-key", "7fbca228870a58a7ff8664b85d0bcb89");
+      myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+      var requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow",
+      };
+
+      fetch(
+            "status",
+            requestOptions
+      )
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.log("error", error));
+
+      return "First Team";
+}
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+      return (
+            <div className="App">
+                  <FirstTeam />
+            </div>
+      );
 }
 
 export default App;
